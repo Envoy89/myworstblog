a82ed1ca4760c77388ac71ce13b1d4698d48cd44
Minyazev Ruzil <ruzil33@gmail.com>
2022-03-22 10:27:22 +0300

    broke one test

--- server/test/models/Tags.test.js
+++ server/test/models/Tags.test.js
@@ -20,7 +20,7 @@ describe('Tag', () => {
         const findTag = await Tag.findOne({name}).exec();
 
         expect(findTag).to.exist;
-        expect(findTag.name).to.equal(name);
+        expect(findTag.name).to.equal("123");
     });
 
     it('can be create correctly many tags', async () => {
